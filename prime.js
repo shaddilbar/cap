6f1e18aa11e2b8aa8749ad67f2718b10e0d0435b
shaddilbar <dilbardeveloper@gmail.com>
2023-08-28 13:05:26 +0530

    added prime no. code

---
+++ prime.js
@@ -0,0 +1,21 @@
+let number = 5;
+
+let fact = 0;
+for(i=1; i<=number; i++)
+{
+    if(number %i==0)
+    
+    {      
+    fact++
+    
+}
+
+}
+if(fact==2)
+{
+    console.log("prime")
+}
+else
+{
+    console.log("Not prime")
+}
